bdce58d21fa38bd19188c61982b2b3031ce185c1
knidarkness <knidarkness@gmail.com>
2019-10-07 02:01:44 +0300

    remove redundant files

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import { levenshteinOnArrayAsync, levenshteinOnArray, levenshtein, killWorkers } from "./index";
-
-levenshteinOnArrayAsync("tst", ["test", "worooos"]).then(r => {
-  console.log("done");
-  console.log(r);
-  killWorkers();
-});
